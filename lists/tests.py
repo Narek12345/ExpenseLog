5d4c40493f6411df33b21d6c1e48dfc405d61488
Narek12345 <narekbayanduryan16@gmail.com>
2025-01-24 16:00:45 +0300

    Wrote a unit test to check the correct filling of the form with the addition of new tasks.

--- lists/tests.py
+++ lists/tests.py
@@ -4,7 +4,13 @@ from django.test import TestCase
 class HomePageTest(TestCase):
 	"""Тест домашней страницы."""
 
-	def test_home_page_returns_correct_html(self):
-		"""Тест: домашняя страница возвращает правильный html."""
+	def test_uses_home_template(self):
+		"""Тест: используется домашний шаблон."""
 		response = self.client.get('/')
+		self.assertTemplateUsed(response, 'home.html')
+
+	def test_can_save_a_POST_request(self):
+		"""Тест: можно сохранить post-запрос."""
+		response = self.client.post('/', data={'item_text': 'A new list item'})
+		self.assertIn('A new list item', response.content.decode())
 		self.assertTemplateUsed(response, 'home.html')
