cc831cf11df9592e49b1f50ca00ac218455cd4d7
Narek12345 <narekbayanduryan16@gmail.com>
2025-01-24 09:37:32 +0300

    The 'home_page' view now returns minimal html.

--- lists/views.py
+++ lists/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import render
+from django.http import HttpResponse
 
 
-def home_page():
+def home_page(request):
 	"""Домашняя страница."""
-	pass
+	response = HttpResponse('<html><title>To-Do lists</title></html>')
+	return response
