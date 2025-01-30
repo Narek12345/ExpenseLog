5d7e3691b182e41b4f4bd15df6fdd7abf0864f8a
Narek12345 <narekbayanduryan16@gmail.com>
2025-01-30 13:15:37 +0300

    Updated the view_list method to allow tests to pass.

--- functional_tests/test_list_item_validation.py
+++ functional_tests/test_list_item_validation.py
@@ -29,10 +29,9 @@ class ItemValidationTest(FunctionalTest):
 		self.browser.find_element(By.ID, 'id_new_item').send_keys(Keys.ENTER)
 
 		# Эдит получает аналогичное предупреждение на странице списка.
-		self.wait_for(
-			self.browser.find_element(By.CSS_SELECTOR, '.has-error').text,
-			"You can't have an empty list item"
-		)
+		self.wait_for(lambda: self.assertEqual(
+			self.browser.find_element(By.CSS_SELECTOR, '.has-error').text, "You can't have an empty list item"
+		))
 
 		# И она может его исправить, заполнив поле неким текстом.
 		self.browser.find_element(By.ID, 'id_new_item').send_keys('Make tea')
