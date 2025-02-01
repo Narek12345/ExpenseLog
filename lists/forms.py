2530de1bbe765e9e11526cfe88258edfcb59b3cc
Narek12345 <narekbayanduryan16@gmail.com>
2025-02-01 10:29:56 +0300

    Created a method for data validation in the form and added a check for duplicates to it.

--- lists/forms.py
+++ lists/forms.py
@@ -39,3 +39,12 @@ class ExistingListItemForm(ItemForm):
 	def __init__(self, for_list, *args, **kwargs):
 		super().__init__(*args, **kwargs)
 		self.instance.list = for_list
+
+
+	def validate_unique(self):
+		"""Проверка уникальности."""
+		try:
+			self.instance.validate_unique()
+		except ValidationError as e:
+			e.error_dict = {'text': [DUPLICATE_ITEM_ERROR]}
+			self._update_errors(e)
