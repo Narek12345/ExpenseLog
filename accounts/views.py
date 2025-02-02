8484a014571be0dd669ef8f7248c56da5022144a
Narek12345 <narekbayanduryan16@gmail.com>
2025-02-02 22:18:31 +0300

    Unit tests related to sending and verifying delivery of the token pass.

--- accounts/views.py
+++ accounts/views.py
@@ -1,14 +1,21 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.core.mail import send_mail
 from django.contrib import messages
+
+from accounts.models import Token
 
 
 def send_login_email(request):
 	"""Отправить сообщение для входа в систему."""
 	email = request.POST['email']
+	token = Token.objects.create(email=email)
+	url = request.build_absolute_uri(
+		reverse('login') + '?token=' + str(token.uid)
+	)
+	message_body = f'Use this link to log in:\n\n{url}'
 	send_mail(
 		'Your login link for SuperLists',
-		'Use this link to log in',
+		message_body,
 		'narekbayanduryan16@gmail.com',
 		[email]
 	)
