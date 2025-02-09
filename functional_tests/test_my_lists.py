6a2f39e54d711707a7f813cf02262f00f8241cb1
Narek <narekbayanduryan16@gmail.com>
2025-02-09 20:13:17 +0300

    Wrote FT for the section 'My lists'.

--- functional_tests/test_my_lists.py
+++ functional_tests/test_my_lists.py
@@ -31,11 +31,51 @@ class MyListsTest(FunctionalTest):
 
 	def test_logged_in_users_lists_are_saved_as_my_lists(self):
 		"""Тест: списки зарегестрированных пользователей сохраняются как 'Мои списки'."""
-		email = 'edith@example.com'
-		self.browser.get(self.live_server_url)
-		self.wait_to_be_logged_out(email)
-
 		# Эдит является зарегестрированным пользователем.
-		self.create_pre_authenticated_session(email)
+		self.create_pre_authenticated_session('edith@example.com')
+
+		# Эдит открывает домашнюю страницу и начинает новый список.
 		self.browser.get(self.live_server_url)
-		self.wait_to_be_logged_in(email)
+		self.add_list_item('Reticulate splines')
+		self.add_list_item('Immanentize eschaton')
+		first_list_url = self.browser.current_url
+
+		# Она замечает ссылку на "Мои списки" в первый раз.
+		self.browser.find_element(By.LINK_TEXT, 'My lists').click()
+
+		# Она видит, что ее список находится там, и он назван на основе первого элемента списка.
+		self.wait_for(
+			lambda:
+				self.browser.find_element(By.LINK_TEXT, 'Reticulate splines')
+		)
+		self.browser.find_element(By.LINK_TEXT, 'Reticulate splines').click()
+		self.wait_for(
+			lambda:
+				self.assertEqual(self.browser.current_url, first_list_url)
+		)
+
+		# Она решает начать еще один список, тчобы только убедиться.
+		self.browser.get(self.live_server_url)
+		self.add_list_item('Click cows')
+		second_list_url = self.browser_current_url
+
+		# Под заголовки "Мои списки" появляется ее новый список.
+		self.browser.find_element(By.LINK_TEXT, 'My lists').click()
+		self.wait_for(
+			lambda:
+				self.browser.find_element(By.LINK_TEXT, 'Click cows')
+		)
+		self.browser.find_element(By.LINK_TEXT, 'Click cows').click()
+		self.wait_for(
+			lambda:
+				self.assertEqual(self.browser.current_url, second_list_url)
+		)
+
+		# Она выходит из системы. Опция "Мои списки" исчезает.
+		self.browser.find_element(By.LINK_TEXT, 'Log out').click()
+		self.wait_for(
+			lambda:
+				self.assertEqual(
+					self.browser.find_elements(By.LINK_TEXT, 'My lists'),
+				)
+		)
