3baf374ed478a163c8efd66fa79343fcc6091075
Narek <narekbayanduryan16@gmail.com>
2025-02-09 20:35:17 +0300

    Wrote a method to add an element to a list.

--- functional_tests/base.py
+++ functional_tests/base.py
@@ -3,6 +3,7 @@ import time
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
+from selenium.webdriver.common.keys import Keys
 from selenium.common.exceptions import WebDriverException
 
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
@@ -78,3 +79,14 @@ class FunctionalTest(StaticLiveServerTestCase):
 		self.browser.find_element(By.NAME, 'email')
 		navbar = self.browser.find_element(By.CSS_SELECTOR, '.navbar')
 		self.assertNotIn(email, navbar.text)
+
+
+	def add_list_item(self, item_text):
+		"""Добавить элемент списка."""
+		num_rows = len(
+			self.browser.find_elements(By.CSS_SELECTOR, '#id_list_table tr')
+		)
+		self.get_item_input_box().send_keys(item_text)
+		self.get_item_input_box().send_keys(Keys.ENTER)
+		item_number = num_rows + 1
+		self.wait_for_row_in_list_table(f'{item_number}: {item_text}')
