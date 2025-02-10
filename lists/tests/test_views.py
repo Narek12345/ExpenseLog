261ad44a9869047731877d282d710fe2dddaa1e3
Narek <narekbayanduryan16@gmail.com>
2025-02-10 19:39:30 +0300

    Wrote a test to check that the correct user is being passed to the template.

--- lists/tests/test_views.py
+++ lists/tests/test_views.py
@@ -2,12 +2,16 @@ from unittest import skip
 
 from django.test import TestCase
 from django.utils.html import escape
+from django.contrib.auth import get_user_model
 
 from lists.models import Item, List
 from lists.forms import (
 	DUPLICATE_ITEM_ERROR, EMPTY_ITEM_ERROR,
 	ExistingListItemForm, ItemForm,
 )
+
+
+User = get_user_model()
 
 
 
@@ -201,8 +205,18 @@ class NewListTest(TestCase):
 
 
 class MyListsTest(TestCase):
+	"""Тест 'моих списков'."""
 
 
 	def test_my_lists_url_renders_my_lists_template(self):
+		"""Тест: url-адрес для 'моих списков' отображает соответствующий шаблон."""
 		response = self.client.get('/lists/users/a@b.com')
 		self.assertTemplateUsed(response, 'my_lists.html')
+
+
+	def test_passes_correct_owner_to_template(self):
+		"""Тест: передается правильный владелец в шаблон."""
+		User.objects.create(email='wrong@owner.com')
+		correct_user = User.objects.create(email='a@b.com')
+		response = self.client.get('/lists/users/a@b.com')
+		self.assertEqual(response.context['owner'], correct_user)
