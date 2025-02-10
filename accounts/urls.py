a1c573495ee9ee468ca060a30ea84a5181066017
Narek <narekbayanduryan16@gmail.com>
2025-02-10 11:00:55 +0300

    Wrote and implemented a logout view.

--- accounts/urls.py
+++ accounts/urls.py
@@ -7,5 +7,5 @@ from accounts import views
 urlpatterns = [
 	path('send_login_email', views.send_login_email, name='send_login_email'),
 	path('login', views.login, name='login'),
-	path('logout/', LogoutView.as_view(next_page='/'), name='logout'),
+	path('logout', views.logout, name='logout'),
 ]
