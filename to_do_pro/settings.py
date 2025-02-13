f6e3f3a85acd95acfbe74ee8389dddf45ba96d99
Narek <narekbayanduryan16@gmail.com>
2025-02-13 18:18:17 +0300

    Deleted the confidential key from code.

--- to_do_pro/settings.py
+++ to_do_pro/settings.py
@@ -10,7 +10,9 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/5.1/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2nv&0q_%y6n3-2s9!k9um%wye0hot*_ui!4*5&9i$@j=d198&1'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
