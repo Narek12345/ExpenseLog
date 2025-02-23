924a5ee87501937122e7424c1a94ada1d4653a94
Narek <narekbayanduryan16@gmail.com>
2025-02-23 08:36:22 +0300

    Changed settings structure.

--- expenselog/settings.py
+++ expenselog/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.1/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-4+c_w&iql#dvn4@yo*u$9$o$hpgs0m4sg)ucqf7+^sv=nl+oni'
+SECRET_KEY = 'django-insecure-#$$ai%o_3stxqz_wc($to18jfyys-^*^mdhw++lleuu-%!q9*b'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
